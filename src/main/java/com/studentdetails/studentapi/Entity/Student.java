ca2d4e7f80a80550f7bfc310ccf367ea3a2fd190
Lokesh0018 <munakalalokesh222@gmail.com>
2025-09-16 20:56:46 +0530

    first commit

--- src/main/java/com/studentdetails/studentapi/Entity/Student.java
+++ src/main/java/com/studentdetails/studentapi/Entity/Student.java
@@ -1,5 +1,93 @@
 package com.studentdetails.studentapi.Entity;
 
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Component
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String branch;
+    private String state;
+    private String country;
+    @Lob
+    private byte[] img;
+    
+    public Student() {
+    }
+
+    public Student(int id, String name, String email, String branch, String state, String country, byte[] img) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.branch = branch;
+        this.state = state;
+        this.country = country;
+        this.img = img;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public byte[] getImg() {
+        return img;
+    }
+
+    public void setImg(byte[] img) {
+        this.img = img;
+    }
 
 }
