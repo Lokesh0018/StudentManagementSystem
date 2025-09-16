f0a94a8f2c2e8a7010c3e0d45d9374350a0cfd59
Lokesh0018 <munakalalokesh222@gmail.com>
2025-09-16 21:28:30 +0530

    Added GetMapping

--- src/main/java/com/studentdetails/studentapi/Controller/StudentController.java
+++ src/main/java/com/studentdetails/studentapi/Controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.studentdetails.studentapi.Controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.studentdetails.studentapi.Entity.Student;
 import com.studentdetails.studentapi.Service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -21,6 +26,14 @@ public class StudentController {
     @GetMapping("/all")
     public ResponseEntity<List<Student>> getAll() {
         return new ResponseEntity<>(studentService.getAll(),HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getById(@PathVariable int id) {
+        Student student = studentService.getById(id);
+        if (student!=null)
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     
 }
--- src/main/java/com/studentdetails/studentapi/Service/StudentService.java
+++ src/main/java/com/studentdetails/studentapi/Service/StudentService.java
@@ -1,5 +1,24 @@
 package com.studentdetails.studentapi.Service;
 
-public class StudentService {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import com.studentdetails.studentapi.Entity.Student;
+import com.studentdetails.studentapi.Repo.StudentRepo;
 
+@Component
+@Service
+public class StudentService {
+    @Autowired
+    StudentRepo studentRepo;
+
+    public List<Student> getAll(){
+        return studentRepo.findAll();
+    }
+
+    public Student getById(int id){
+        return studentRepo.findById(id).orElse(null);
+    }
+    
 }
