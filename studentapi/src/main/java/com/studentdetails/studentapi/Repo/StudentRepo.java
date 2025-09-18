a40b86dc43ec982528ef8043ee7d7bb058e6d1e5
Lokesh0018 <munakalalokesh222@gmail.com>
2025-09-18 17:56:39 +0530

    Added all project files

---
+++ studentapi/src/main/java/com/studentdetails/studentapi/Repo/StudentRepo.java
@@ -0,0 +1,13 @@
+package com.studentdetails.studentapi.Repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.studentdetails.studentapi.Entity.Student;
+
+public interface StudentRepo extends JpaRepository<Student,Integer>{
+    
+    public Optional<Student> findByName(String name);
+
+}
