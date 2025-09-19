00f95515601b7c2c829d7d7f8d338d512be37f04
Lokesh0018 <munakalalokesh222@gmail.com>
2025-09-19 21:11:52 +0530

    NavBar ....

--- FrontEnd/admin/src/Navigation/Nav.js
+++ FrontEnd/admin/src/Navigation/Nav.js
@@ -1,28 +1,32 @@
-import React from "react";
-import { TbCircleLetterAFilled,TbSmartHome ,TbUsers,TbUser  } from "react-icons/tb";
+import React, { useState } from "react";
+import { TbCircleLetterAFilled, TbSmartHome, TbUsers, TbUser } from "react-icons/tb";
+import { CgClose } from "react-icons/cg";
 
 const Nav = () => {
+    const [pressed, setPressed] = useState(false);
+    const [select, setSelect] = useState("home");
+
     return (
-        <div className="nav">
-            <div className="bar">
-                <div className="icContainer">
-                    <TbCircleLetterAFilled className="icon" />
-                    <span className="logo">Admin</span>
+        <div className={`nav ${pressed ? "expand" : "collapse"}`}>
+            <div className="navItems">
+                <div className="icContainer" onClick={() => setPressed(!pressed)}>
+                    <TbCircleLetterAFilled className="icon"/>
+                    <CgClose className={pressed ? "ic display" : "icon hide"} onClick={() => setPressed(false)}/>
                 </div>
-                <ul>
-                    <li>
-                        <TbSmartHome /> Home
+                <ul className="navList">
+                    <li className={`items ${select === "home" ? "itemSelect":""}`} onClick={() => setSelect("home")}>
+                        <TbSmartHome className="ic"/><span className={pressed ? "display" : "hide"}>Home</span>
                     </li>
-                    <li>
-                        <TbUsers />Students
+                    <li className={`items ${select === "students" ? "itemSelect":""}`} onClick={() => setSelect("students")}>
+                        <TbUsers className="ic"/><span className={pressed ? "display" : "hide"}>Students</span>
                     </li>
-                    <li>
-                       <TbUser /> Student
+                    <li className={`items ${select === "student" ? "itemSelect":""}`} onClick={() => setSelect("student")}>
+                        <TbUser className="ic"/><span className={pressed ? "display" : "hide"}>Student</span>
                     </li>
                 </ul>
             </div>
         </div>
     );
-}
+};
 
 export default Nav;
